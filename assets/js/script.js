052702d176a16bd5d8267da445afb1f130d90894
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-07-20 22:20:37 -0400

    Tooltips en formulario.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
-//import '../../node_modules/jquery/dist/jquery.min.js';
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+/* 
+$(document).ready(function(){
+    $("#botonEnviar").click(function () {
+        alert("Mensaje enviado.");
+    });
+});
+ */

23b34e8886d41a1a1cb1c94db3d5f646d5090df1
plazasoto <jarrettsguitarsfactory@gmail.com>
2024-07-20 22:38:26 -0400

    Modal términos y condiciones.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,9 @@ import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-/* 
 $(document).ready(function(){
+
     $("#botonEnviar").click(function () {
         alert("Mensaje enviado.");
     });
 });
- */
